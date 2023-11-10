4b6280843eba968349fa93a9ac124cca40b073a2
Hikmet534 <hikmethan.kolay@gmail.com>
2023-11-10 23:37:42 +0300

    Show records and register records for Crop and livestock management

--- src/farm_management_app/src/farm_management_app.cpp
+++ src/farm_management_app/src/farm_management_app.cpp
@@ -8,10 +8,15 @@ fstream File;
 
 int main() {
   int choice;
+  int choice_a;
+  int choice_b;
+  int choice_c;
+  int choice_d;
+  bool app_running = true;
 
   do {
-    // Show Main menu
-    cout << "Main menu\n";
+    //Main menu
+    cout << "-----------Main menu-----------\n";
     cout << "1)Crop and livestock management\n";
     cout << "2)Harvesting and producing planning\n";
     cout << "3)Equipment and vehicle maintenance\n";
@@ -22,38 +27,75 @@ int main() {
 
     switch (choice) {
       case 1:
-        cout << "Crop and livestock management\nWhat operation do you want to do?\n";
+        cout << "\n--------Crop and livestock management--------\n";
+        cout << "What operation do you want to do ? \n";
         cout << "1-)Show Records\n";
         cout << "2-)Register Record\n";
         cout << "3-)Update Record\n";
         cout << "4-)Delete Record\n",
-             cin >> choice;
+             cout << "5-)Return previous menu\n",
+             cout << "Make a choice(1 - 5): ";
+        cin >> choice_a;
 
-        switch (choice) {
+        switch (choice_a) {
           case 1: {
-            file_read("crop_livestock_record.txt");
+            string record_kind_to_see;
+            cout << "Which record do you want to see? Livestock/Crop?: ";
+            cin >> record_kind_to_see;
+
+            if (record_kind_to_see == "Crop") {
+              cout << "--------------------------------\n";
+              file_read("crop_records.txt");
+              cout << "--------------------------------\n";
+            } else if (record_kind_to_see == "Livestock") {
+              cout << "--------------------------------\n";
+              file_read("livestock_records.txt");
+              cout << "--------------------------------\n";
+            } else {
+              cout << "Please sellect a correct option\n";
+              continue;
+            }
+
             break;
           }
 
           case 2: {
             string record;
-            string record_kind;
-            cout << "What kind of record do you want to enter? Livestock/Crop?";
-            cin >> record_kind;
+            string record_kind_to_register;
+            cout << "What kind of data do you want to enter? Livestock/Crop?: ";
+            cin >> record_kind_to_register;
 
-            if (record_kind == "Crop") {
-              File.open("crop_records.txt", ios::out | ios::in);
+            if (record_kind_to_register == "Crop") {
               record = "Test";
+              File.open("crop_records.txt", ios::out | ios::in);
 
               if (File.is_open()) {
                 file_append("crop_records.txt", record);
+                cout << "Data succesfully recored ";
                 break;
               } else {
-                file_write("crop_records.txt", "CROP TYPE / PLANTING DATE / HARVEST DATE / CULTIVATION AREA");
+                file_write("crop_records.txt", "CROP TYPE / DATE OF PLANTING / DATE OF HARVESTING / CULTIVATED FIELD / EXPECTED QUANTITY");
                 file_append("crop_records.txt", record);
+                cout << "Data succesfully recored ";
                 break;
               }
-            } else if (record_kind == "Livestock") {
+            } else if (record_kind_to_register == "Livestock") {
+              record = "Test";
+              File.open("livestock_records.txt", ios::out | ios::in);
+
+              if (File.is_open()) {
+                file_append("livestock_records.txt", record);
+                cout << "Data succesfully recored ";
+                break;
+              } else {
+                file_write("livestock_records.txt", "LIVESTOCK TYPE / EAR TAG NUMBER / DATE OF BIRTH / DATE OF DEATH / CAUSE OF DEATH / GIVEN FEED ");
+                file_append("livestock_records.txt", record);
+                cout << "Data succesfully recored ";
+                break;
+              }
+            } else {
+              cout << "Please sellect a correct option\n";
+              continue;
             }
           }
 
@@ -61,6 +103,10 @@ int main() {
           }
 
           case 4: {
+          }
+
+          case 5: {
+            break;
           }
         }
 
@@ -82,14 +128,14 @@ int main() {
       }
 
       case 5: {
-        cout << "You are going to exit.\n";
+        app_running = false;
         break;
       }
 
       default:
         cout << "Please try again.";
     }
-  } while (choice != 5);
+  } while (app_running);
 
   return 0;
 }
