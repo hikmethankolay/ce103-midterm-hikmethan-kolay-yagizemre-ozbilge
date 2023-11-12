9bdab5e2b4816d2a3177828b67a2132f325bfe97
Hikmet534 <hikmethan.kolay@gmail.com>
2023-11-12 20:00:34 +0300

    .gitignore changes and .ba file bug fix

--- 8-build-txt-files-for-test-windows.bat
+++ 8-build-txt-files-for-test-windows.bat
@@ -70,4 +70,34 @@ echo 4-)TEXT STRING4>> test5.txt
 
 cd ../../
 
+echo 0-)TEXT STRING0> test1.txt
+echo 1-)TEXT STRING1>> test1.txt
+echo 2-)TEXT STRING2>> test1.txt
+echo 3-)TEXT STRING3>> test1.txt
+echo 4-)TEXT STRING4>> test1.txt
+
+echo 0-)TEXT STRING0> test2.txt
+echo 1-)TEXT STRING1>> test2.txt
+echo 2-)TEXT STRING2>> test2.txt
+echo 3-)TEXT STRING3>> test2.txt
+echo 4-)TEXT STRING4>> test2.txt
+
+echo 0-)TEXT STRING0> test3.txt
+echo 1-)TEXT STRING1>> test3.txt
+echo 2-)TEXT STRING2>> test3.txt
+echo 3-)TEXT STRING3>> test3.txt
+echo 4-)TEXT STRING4>> test3.txt
+
+echo 0-)TEXT STRING0> test4.txt
+echo 1-)TEXT STRING1>> test4.txt
+echo 2-)TEXT STRING2>> test4.txt
+echo 3-)TEXT STRING3>> test4.txt
+echo 4-)TEXT STRING4>> test4.txt
+
+echo 0-)TEXT STRING0> test5.txt
+echo 1-)TEXT STRING1>> test5.txt
+echo 2-)TEXT STRING2>> test5.txt
+echo 3-)TEXT STRING3>> test5.txt
+echo 4-)TEXT STRING4>> test5.txt
+
 echo txt files created successfully
